bc9c164a566b7f98bf0846fb2d23f06093608772
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-20 13:21:32 +0100

    starting full stack post lab

---
+++ lab_full_stack_post_birds_start/client/src/components/SightingsGrid.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+	<div id="sightingsGrid">
+		<sighting v-for="sighting in sightings" :sighting="sighting" />
+	</div>
+</template>
+
+<script>
+import Sighting from './Sighting';
+
+export default {
+	name: 'sightings-grid',
+	components: {
+		'sighting': Sighting
+	},
+	props: ['sightings']
+}
+</script>
+
+<style lang="css" scoped>
+#sightingsGrid {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+}
+
+h2 {
+	padding: 0;
+	margin: 0;
+}
+</style>
